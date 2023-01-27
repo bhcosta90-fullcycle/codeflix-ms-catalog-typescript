b9e853ef44ae73f3a9f0bb3aeffffc22f8719e95
Bruno Costa <bruno.costa@dafiti.com.br>
2023-01-27 11:09:06 -0300

    não finalizado

--- src/category/domain/entity/category.entity.spec.ts
+++ src/category/domain/entity/category.entity.spec.ts
@@ -1,8 +1,71 @@
 import { Category } from "./category.entity";
+import { omit } from "lodash";
 
-describe("CategoryEntity Unit Test", () => {
-  it("Constructor", () => {
-    const entity = new Category("movie");
-    expect(entity.name).toBe("movie");
+describe("Category Unit Test", () => {
+  describe("Constructor", () => {
+    it("passed all parameters to the constructor", () => {
+      const created_at = new Date();
+
+      const entity = new Category({
+        name: "movie",
+        description: "some description",
+        is_active: false,
+        created_at,
+      });
+
+      expect(entity.props).toStrictEqual({
+        name: "movie",
+        description: "some description",
+        is_active: false,
+        created_at,
+      });
+    });
+
+    it("passed a minimum parameter to the constructor", () => {
+      const entity = new Category({
+        name: "movie 2",
+      });
+
+      expect(omit(entity.props, "created_at")).toStrictEqual({
+        name: "movie 2",
+        description: null,
+        is_active: true,
+      });
+
+      expect(entity.created_at).toBeInstanceOf(Date);
+    });
+
+    describe("Getter and setter", () => {
+      const created_at = new Date();
+
+      const entity = new Category({
+        name: "movie",
+        description: "some description",
+        is_active: false,
+        created_at,
+      });
+
+      it("name prop", () => {
+        expect(entity.name).toBe("movie");
+        entity["name"] = "other movie";
+        expect(entity.name).toBe("other movie");
+      });
+
+      it("description prop", () => {
+        expect(entity.description).toBe("some description");
+        entity["description"] = undefined;
+        expect(entity.description).toBeNull();
+      });
+
+      it("is_active prop", () => {
+        expect(entity.is_active).toBeFalsy();
+        entity["is_active"] = undefined;
+        expect(entity.is_active).toBeTruthy();
+      });
+
+      it("created_at prop", () => {
+        expect(entity.created_at).toBe(created_at);
+      });
+    });
   });
 });
--- src/category/domain/entity/category.entity.ts
+++ src/category/domain/entity/category.entity.ts
@@ -1,3 +1,42 @@
 export class Category {
-  constructor(public name: string) {}
+  constructor(public props: CategoryType) {
+    this.description = this.props.description;
+    this.is_active = this.props.is_active;
+    this.props.created_at = this.props.created_at ?? new Date();
+  }
+
+  get name() {
+    return this.props.name;
+  }
+
+  get description() {
+    return this.props.description;
+  }
+
+  get is_active() {
+    return this.props.is_active;
+  }
+
+  get created_at() {
+    return this.props.created_at;
+  }
+
+  private set name(name){
+    this.props.name = name;
+  }
+
+  private set description(description){
+    this.props.description = description ?? null;
+  }
+
+  private set is_active(is_active){
+    this.props.is_active = is_active ?? true;
+  }
 }
+
+export type CategoryType = {
+  name: string;
+  description?: string;
+  is_active?: boolean;
+  created_at?: Date;
+};
