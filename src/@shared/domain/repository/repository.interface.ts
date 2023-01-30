22b47aff1afe6c61dedabc739a5ec41cf68c28a7
Bruno Costa <bruno.costa@dafiti.com.br>
2023-01-30 15:16:56 -0300

    iniciando o metodo do output

--- src/@shared/domain/repository/repository.interface.ts
+++ src/@shared/domain/repository/repository.interface.ts
@@ -99,10 +99,56 @@ export class SearchParams<Filter = string> {
   }
 }
 
+export type SearchResultProps<E extends Entity<any>, Filter> = {
+  items: E[];
+  total: number;
+  current_page: number;
+  per_page: number;
+  sort: string | null;
+  sort_dir: string | null;
+  filter: Filter;
+};
+
+export class SearchResult<E extends Entity<any>, Filter = string> {
+  readonly items: E[];
+  readonly total: number;
+  readonly current_page: number;
+  readonly per_page: number;
+  readonly last_page: number;
+  readonly sort: string;
+  readonly sort_dir: string;
+  readonly filter: Filter | null;
+
+  constructor(props: SearchResultProps<E, Filter>) {
+    this.items = props.items;
+    this.total = props.total;
+    this.current_page = props.current_page;
+    this.per_page = props.per_page;
+    this.sort = props.sort;
+    this.sort_dir = props.sort_dir;
+    this.filter = props.filter;
+    this.last_page = Math.ceil(this.total / this.per_page);
+  }
+
+  toJSON() {
+    return {
+      items: this.items,
+      total: this.total,
+      current_page: this.current_page,
+      per_page: this.per_page,
+      sort: this.sort,
+      sort_dir: this.sort_dir,
+      filter: this.filter,
+      last_page: this.last_page,
+    };
+  }
+}
+
 export interface SearchableRepositoryInterface<
   E extends Entity<any>,
+  Filter = string,
   SearchInput = SearchParams,
-  SearchOutput = null
+  SearchOutput = SearchResult<E, Filter>
 > extends RepositoryInterface<E> {
   search(props: SearchInput): Promise<SearchOutput>;
 }
