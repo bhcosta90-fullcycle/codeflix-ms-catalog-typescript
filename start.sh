c5213139677f44fa51bc9ba3eb8015815d3f0ae7
Bruno Costa <bruno.costa@dafiti.com.br>
2023-02-27 14:58:45 -0300

    finalizado

--- start.sh
+++ start.sh
@@ -4,6 +4,10 @@ if [ ! -f "./src/@shared/.env.testing" ]; then
     cp ./src/@shared/.env.testing.example ./src/@shared/.env.testing
 fi
 
+if [ ! -f "./src/nestjs/src/envs/.env" ]; then
+    cp ./src/nestjs/src/envs/.env.example ./src/nestjs/src/envs/.env
+fi
+
 yarn install
 
 yarn run dev
